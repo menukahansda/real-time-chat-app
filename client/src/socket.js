aa46d9eb276739cbf12809c6ce671a8a8bdc16e0
menukahansda <menukahansda199@gmail.com>
2026-07-02 23:54:16 +0530

    add logs to check connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -5,3 +5,11 @@ const URL = import.meta.env.VITE_APP_SOCKET_URL || "http://localhost:3000";
 export const socket = io(URL, {
   autoConnect: false,
 });
+
+socket.on("connect", () => {
+  console.log("✅ Connected:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.error("❌ Connect error:", err.message);
+});
