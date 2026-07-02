93bcaf91a1d36142532ef64caf261b0bb1746b10
menukahansda <menukahansda199@gmail.com>
2026-07-02 13:39:07 +0530

    chore: set up development environment

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen text-3xl font-bold">
+        <div>
+          Real Time Chat App
+        </div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
