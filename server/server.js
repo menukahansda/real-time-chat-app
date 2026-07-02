017b6c5235a2c45a7e3866b48aa2c9a38bc34080
menukahansda <menukahansda199@gmail.com>
2026-07-02 15:18:23 +0530

    feat: add get endpoint for fetching messages

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -15,10 +16,15 @@ const io = new Server(server, {
 
 let messages = [];
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Home page. Server working!");
+});
+
+app.get("/messages", (req, res) => {
+    res.json(messages);
 });
 
 io.on("connection", (socket) => {
