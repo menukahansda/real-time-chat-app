138a8439c2325919a7a2568344f35bba36685554
menukahansda <menukahansda199@gmail.com>
2026-07-02 14:31:50 +0530

    feat: implement join, sendMessage, and receiveMessage socket events

--- server/server.js
+++ server/server.js
@@ -7,12 +7,46 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin : "*"
+    }
+})
+
+let messages = [];
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Home page. Server working!");
 });
+
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("join", (username) => {
+        socket.data.username = username;
+        console.log(`${username} joined the chat. Socket ID: ${socket.id}`);
+    });
+
+    socket.on("sendMessage", ({ text }) => {
+        const message = {
+            username: socket.data.username,
+            text,
+            timestamp: Date.now(),
+            socketId: socket.id
+        };
+
+        messages.push(message);
+        io.emit("receiveMessage", message);
+   
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
