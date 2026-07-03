3d8dc629f2280c5b08f9284a1eea4679ef29e822
menukahansda <menukahansda199@gmail.com>
2026-07-03 22:23:48 +0530

    feat: wake backend on app load

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Login from "./components/Login";
 import ChatRoom from "./components/ChatRoom";
 
 export default function App() {
   const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_APP_SOCKET_URL}/health`)
+    .then((res)=>{
+      if(res.ok){
+        console.log("SERVER URL REACHABLE");
+      }
+    })
+    .catch(() => {
+      console.error("SERVER URL NOT REACHABLE");
+    });
+  }, []);
 
   return username ? (
     <ChatRoom username={username} />
